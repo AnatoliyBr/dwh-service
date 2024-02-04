e5577f48c63633599f215c9930cb53f1f38a0a10
AnatoliyBr <anatoliyb2000@mail.ru>
2024-02-04 20:22:30 +0300

    fix customtime type

--- internal/entity/customtime.go
+++ internal/entity/customtime.go
@@ -18,18 +18,17 @@ func (t CustomTime) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`"%s"`, t.Time.Format(defaultLayout))), nil
 }
 
-func (t *CustomTime) UnMarshalJSON(data []byte) error {
-
+func (t *CustomTime) UnmarshalJSON(data []byte) error {
 	// from json doc: by convention, unmarshalers implement UnmarshalJSON([]byte("null")) as a no-op
 	if bytes.Equal(data, []byte("null")) {
 		return nil
 	}
 
-	time, err := time.Parse(defaultLayout, string(data))
+	time, err := time.Parse(`"`+defaultLayout+`"`, string(data))
 	if err != nil {
 		return err
 	}
 
-	t = &CustomTime{time}
+	t.Time = time
 	return nil
 }
